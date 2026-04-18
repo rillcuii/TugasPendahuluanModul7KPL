cc09392854442ab92eda00e32eac304e323c9412
khalilgtgbgtkhalilgtg@gmail.com <khaaleell>
2026-04-18 22:03:58 +0700

    Menambahkan class KuliahMahasiswa_103082400033

---
+++ CODE_TP_MODUL7/KuliahMahasiswa_103082400033.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CODE_TP_MODUL7
+{
+    public class MataKuliah
+    {
+        public string kode { get; set; }
+        public string nama { get; set; }
+    }
+    public class KuliahMahasiswa_103082400033
+    {
+        public List<MataKuliah> Matkul { get; set; }
+
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("tp7_2_103082400033");
+            KuliahMahasiswa_103082400033 kuliah = JsonSerializer.Deserialize<KuliahMahasiswa_103082400033>(jsonString);
+
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            for (int i = 0; i < kuliah.Matkul.Count; i++)
+            {
+                Console.WriteLine($"MK {i + 1} {kuliah.Matkul[i].kode} - {kuliah.Matkul[i].nama}");
+            }
+        }
+    }
+}
