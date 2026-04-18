fae0785ead175614ce688cf2fdd5b716e490e3c9
khalilgtgbgtkhalilgtg@gmail.com <khaaleell>
2026-04-18 21:39:12 +0700

    menambah class DataMahasiswa_103082400033

---
+++ CODE_TP_MODUL7/DataMahasiswa_103082400033.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace CODE_TP_MODUL7
+{
+    public class DataMahasiswa_103082400033
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public string fakultas { get; set; }
+
+        public void ReadJSON()
+        {
+
+            string jsonString = File.ReadAllText("tp7_1_103082400033");
+            DataMahasiswa_103082400033 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_103082400033>(jsonString);
+
+            Console.WriteLine($"Nama {mahasiswa.nama} dengan nim {mahasiswa.nim} dari fakultas {mahasiswa.fakultas}");
+        }
+    }
+}
