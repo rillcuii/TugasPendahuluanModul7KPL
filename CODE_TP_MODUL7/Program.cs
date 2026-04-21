42518fafadf8e0078b791620362127ff7f1d212f
WongMekz <veryandra.putra@gmail.com>
2026-04-21 22:32:28 +0700

    menambahkan kelas "DataMahasiswa103082400042"

--- CODE_TP_MODUL7/Program.cs
+++ CODE_TP_MODUL7/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using CODE_TP_MODUL7; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instansiasi class dan panggil method ReadJSON [cite: 191-192]
+        DataMahasiswa103082400042 dataMhs = new DataMahasiswa103082400042();
+        dataMhs.ReadJSON();
+    }
+}
